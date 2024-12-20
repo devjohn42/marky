6eced10c3248b37a24c34ed0779e4f9a03508392
devjohn42 <devjohncb@gmail.com>
2024-12-20 00:43:29 -0300

    :lipstick: feat: initial tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/renderer/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Fira Sans', 'sans-serif'],
+      },
+    },
+    colors: {
+      raisin: '#23232F',
+      moonstone: '#74B1BE',
+      raisin_dark: '#20202D',
+      alice: '#E3EDF7',
+      carmine: '#902424',
+    },
+    backgroundImage: {
+      wavy: 'url(wavy-lines.svg)',
+      logo: 'url(logo.svg)',
+    },
+  },
+  plugins: [],
+}
