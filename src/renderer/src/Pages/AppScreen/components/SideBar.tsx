6d7d6e7d26c010a9e509fff76c113c166607e4bc
devjohn42 <devjohncb@gmail.com>
2025-01-07 17:37:09 -0300

    :sparkles: feat: creates a conditional if the list of markdown notes is empty

--- src/renderer/src/Pages/AppScreen/components/SideBar.tsx
+++ src/renderer/src/Pages/AppScreen/components/SideBar.tsx
@@ -6,7 +6,10 @@ type AsideProps = ComponentProps<'aside'>
 const SideBar = ({ className, children, ...props }: AsideProps) => {
   return (
     <aside
-      className={cn('w-[25%] px-2 pt-3 flex flex-col gap-3', className)}
+      className={cn(
+        'w-[25%] bg-raisin px-2 pt-3 flex flex-col gap-3 shadow-[4px_0px_8px_-8px_rgb(0,0,0)] shado',
+        className,
+      )}
       {...props}
     >
       {children}
--- src/renderer/src/Pages/AppScreen/components/ui/NotePreviewList.tsx
+++ src/renderer/src/Pages/AppScreen/components/ui/NotePreviewList.tsx
@@ -1,6 +1,8 @@
 import { ComponentProps } from 'react'
+import { isEmpty } from 'lodash'
 import { NotePreview } from './NotePreview'
 import { useMarkdownNotesList } from '@renderer/hooks/useMarkdownNotesList'
+import { cn } from '@renderer/utils'
 
 type NotePreviewListProps = ComponentProps<'ul'> & {
   onSelect?: () => void
@@ -8,12 +10,22 @@ type NotePreviewListProps = ComponentProps<'ul'> & {
 
 export const NotePreviewList = ({
   onSelect,
+
   ...props
 }: NotePreviewListProps) => {
   const { notes, selectedNoteIndex, handleNoteSelect } = useMarkdownNotesList({
     onSelect,
   })
 
+  if (!notes) return null
+
+  if (isEmpty(notes)) {
+    return (
+      <ul className={cn('text-center pt-3')} {...props}>
+        <span className="text-alice">No Notes Yet!</span>
+      </ul>
+    )
+  }
   return (
     <div className="overflow-y-auto -mr-2 mb-1">
       <ul {...props}>
